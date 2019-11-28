e723bf2b3c8788057dec77fb4d522554cc37c070
youyfeng <youyfeng@gmail.com>
2019-11-28 22:51:05 +0800

    tiger

---
+++ pages/game/tiger.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<!-- https://segmentfault.com/a/1190000019644395?utm_source=tag-newest -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
